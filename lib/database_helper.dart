7ea96cc4cd0c9e7b87cb32ad0678c9b519d6b977
Hina Khan <47082795+hinaabbaskhan@users.noreply.github.com>
2024-01-28 15:57:32 +0500

    get database

--- lib/database_helper.dart
+++ lib/database_helper.dart
@@ -9,4 +9,23 @@ class DatabaseHelper {
   static final DatabaseHelper databaseHelper =
       DatabaseHelper._privateConstructor();
 
+  static Database? _database;
+
+  Future<Database?> get database async {
+    // database create
+    Directory directory = await getApplicationDocumentsDirectory();
+    _database = await openDatabase(directory.path + '/myDatabase.db',
+        version: 1, onCreate: _onCreate);
+    return _database;
+  }
+
+  _onCreate(Database db, int version) {
+    db.execute('''
+      CREATE TABLE Student (
+      id INTEGER PRIMARY KEY,
+      name TEXT
+      )
+      ''');
+  }
+
 }
