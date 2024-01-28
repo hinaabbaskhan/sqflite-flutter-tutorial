e55d8a0402554a1ef592329a45f3511f5e49c5ac
Hina Khan <47082795+hinaabbaskhan@users.noreply.github.com>
2024-01-28 15:57:21 +0500

    creat a _privateConstructor and  instance of helper class

--- lib/database_helper.dart
+++ lib/database_helper.dart
@@ -4,4 +4,9 @@ import 'package:path_provider/path_provider.dart';
 import 'package:sqflite/sqflite.dart';
 
 class DatabaseHelper {
+  // singleton pattern
+  DatabaseHelper._privateConstructor();
+  static final DatabaseHelper databaseHelper =
+      DatabaseHelper._privateConstructor();
+
 }
